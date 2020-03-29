7a57a5cf974d10f4ad545ad74865c6ee57904094
Alex Lamb <alexlamb@Earl.local>
2020-03-29 13:05:20 -0700

    optimization working

--- py/helper.py
+++ py/helper.py
@@ -10,6 +10,9 @@ import pandas as pd
 import numpy as np
 from datetime import datetime as dt
 from datetime import timedelta
+from scipy.optimize import minimize
+import pulp as pl
+import collections as cl
 
 # =============================================================================
 # Helper functions
@@ -43,42 +46,159 @@ def generate_availability(shifts, avail_data, employee_data):
                                (shift_avail['end_hour'] < shift_avail['end_avail_time']))
     
     shift_avail = shift_avail[shift_avail['is_avail'] == True]
-    shift_avail = shift_avail[['employee_name', 'index']]
-    shift_avail = pd.DataFrame(shift_avail.groupby('employee_name')['index'].apply(lambda x: list(x)))
+    shift_avail['duration'] = shift_avail['end_hour'] - shift_avail['start_hour']
+    shift_avail = shift_avail[['employee_name', 'pos_index', 'duration']]
+    shift_avail = pd.DataFrame(shift_avail.groupby('employee_name').agg({'pos_index': lambda x: list(x),
+                                                                         'duration': lambda x: list(x)}))
     shift_avail = shift_avail.merge(employee_data, how = 'left', on = 'employee_name').set_index('employee_name')
     
     return shift_avail.to_dict(orient = 'index')
 
-def generate_hours(start, end):
-    # Create all hours between these two dates
-    date_hour_range = pd.date_range(start=start, end=end, freq='H')[:-1]
+def min_func(x, shift_lengths, shift_starts, labor_req, labor_req_starts):
     
-    df = pd.DataFrame([date_hour_range , 
-                          date_hour_range.day_name(), 
-                          date_hour_range.hour]).T.reset_index()
-    df.columns = ['timeslot', 'datetime', 'day_of_week', 'hour']
-    return df
+    # Determine which labor req entries are covered by which shifts for constraints
+    shifts_in_labor_req = []
+    for i in range(len(labor_req)):
+        shifts_in = []
+        for j in range(len(shift_lengths)):
+            if((shift_starts[j] <= labor_req_starts[i]) & ((shift_starts[j] + shift_lengths[j]) > labor_req_starts[i])):
+                shifts_in.append(j)
+        shifts_in_labor_req.append(shifts_in)
 
-def generate_availability_old(df_hours, df_employee, df_avail):
-    # Find all datetimes that an employee is available
-    df = df_hours.merge(df_avail, 
-                        how = 'outer', 
-                        on = 'day_of_week')
-  
-    df['avail'] = np.where((df['start_avail_time'] <= df['hour']) 
-                            & (df['end_avail_time'] >= df['hour']), 
-                            1, 0)
-    df.drop(['start_avail_time', 'end_avail_time', 'day_of_week', 'datetime', 'hour'], axis = 1, inplace = True)
-    df = df.merge(df_employee,
-                  how = 'left',
-                  on = 'employee_name').drop(['employee_email'], axis = 1)
-    df.set_index(['timeslot', 'employee_name'], inplace = True)
-    return df
-
-def generate_hourly_need(df_hours, df_labor):
-    df = df_hours.merge(df_labor,
-                        how = 'left',
-                        on = ['day_of_week', 'hour'])
-    df.drop(['day_of_week', 'hour'], axis = 1, inplace = True)
-    return df
+    diffs = []
+    for i in range(len(labor_req)):
+        shift_req = 0
+        for j in shifts_in_labor_req[i]:
+            shift_req = shift_req + x[j]
+        diffs.append(shift_req/labor_req[i])
         
+    return np.mean(diffs)
+        
+def build_constraints(shift_lengths, shift_starts, labor_req, labor_req_starts):   
+
+    # Determine which labor req entries are covered by which shifts for constraints
+    shifts_in_labor_req = []
+    for i in range(len(labor_req)):
+        shifts_in = []
+        for j in range(len(shift_lengths)):
+            if((shift_starts[j] <= labor_req_starts[i]) & ((shift_starts[j] + shift_lengths[j]) > labor_req_starts[i])):
+                shifts_in.append(j)
+        shifts_in_labor_req.append(shifts_in)
+    
+    # Dynamically build constraints
+    constraints = []
+    for i in range(len(labor_req)):
+        if (len(shifts_in_labor_req[i]) > 0):
+            constraint =  'lambda x: ' + str(-1 * labor_req[i])
+            for j in shifts_in_labor_req[i]:
+                constraint = constraint + ' + x[' + str(j) + '] '
+            constraints.append(constraint)
+    
+    cons = ()
+    for eq in constraints:
+        cons = cons + ({'type': 'ineq', 'fun': eval(eq)},)
+    
+    for i in range(len(shift_starts)):
+        con = 'lambda x: x[' + str(i) + ']'
+        cons = cons + ({'type': 'ineq', 'fun': eval(con)},)
+
+    return cons
+
+
+def gen_labor(shift_starts,  shift_lengths, labor_req, labor_req_starts):
+
+    cons = build_constraints(shift_lengths, shift_starts, labor_req, labor_req_starts)
+    x = [np.mean(labor_req)] * len(shift_lengths)
+    res = minimize(min_func, x, args = (shift_lengths, shift_starts, labor_req, labor_req_starts), constraints=cons, method='SLSQP', options={'disp': False})
+    labor = np.array(np.round(res.x))
+
+    return labor
+
+def optimize_shifts(shifts_, labor_):
+    
+    shift_starts = list(shifts_['start_hour'])
+    shift_lengths = list(shifts_['end_hour'] - shifts_['start_hour'])
+    
+    labor_req = list(labor_['labor_need'])
+    labor_req_starts = list(labor_['hour'])
+            
+    shift_days = list(set(shifts_.day_of_week.unique()))
+    daily_shifts = pd.DataFrame()
+    for day in shift_days:
+        tmp = shifts_[shifts_['day_of_week'] == day]
+        tmp1 = labor_[labor_['day_of_week'] == day]
+    
+        shift_starts = list(tmp['start_hour'])
+        shift_lengths = list(tmp['end_hour'] - tmp['start_hour'])
+        
+        labor_req = list(tmp1['labor_need'])
+        labor_req_starts = list(tmp1['hour'])
+        
+        if((len(labor_req) > 0) & (sum(np.round(labor_req)) > 0)):
+            reqs = gen_labor(shift_starts, shift_lengths, labor_req, labor_req_starts)
+            tmp['req'] = reqs
+            daily_shifts = pd.concat([daily_shifts, tmp[['index', 'req']]], axis = 0)
+    
+    shifts_ = shifts_.merge(daily_shifts, how = 'left', on = 'index')
+    shifts_ = shifts_[shifts_['req'] > 0].reset_index() \
+                                      .drop('level_0', axis = 1) \
+                                      .reset_index() \
+                                      .rename(columns = {'level_0':'pos_index'}) 
+    
+    return shifts_
+
+def optimize_assignment(shifts_, avail_):
+    
+    shift_reqs = list(shifts_['req'])
+    
+    prob = pl.LpProblem("roster", pl.LpMinimize)
+
+    cost = []
+    vars_by_shift = cl.defaultdict(list)
+    vars_by_worker = cl.defaultdict(list) #max shifts
+    
+    for worker, info in avail_.items():
+        for shift in info['pos_index']:
+            worker_var = pl.LpVariable("%s_%s" % (worker, shift), 0, 1, pl.LpInteger)
+            vars_by_shift[shift].append(worker_var)
+            cost.append(worker_var * (info['hourly_rate']* info['duration']))
+    
+    prob += sum(cost)
+    
+    for shift, requirement in enumerate(shift_reqs):
+        prob += sum(vars_by_shift[shift]) >= requirement
+    
+    status = prob.solve()
+    print("Result:", pl.LpStatus[status])
+    results = []
+    for shift, vars in vars_by_shift.items():
+        results.append({
+            "shift": shift,
+            "workers": [var.name for var in vars if var.varValue == 1],
+        })
+
+    return results
+
+def format_results(results_, employee_data, shifts):
+    results_ = pd.DataFrame(results_)
+    lst_col = 'workers'
+    results_ = pd.DataFrame({
+          col:np.repeat(results_[col].values, results_[lst_col].str.len())
+          for col in results_.columns.drop(lst_col)}
+        ).assign(**{lst_col:np.concatenate(results_[lst_col].values)})[results_.columns]
+    
+    results_['workers'] = results_['workers'].apply(lambda x: x.split('_')[0])
+    results_ = results_.merge(employee_data, how = 'left', left_on = 'workers', right_on = 'employee_name')
+    results_ = results_.merge(shifts, how = 'left', left_on = 'shift', right_on = 'pos_index')
+    results_['duration'] = results_['end_hour'] - results_['start_hour']
+    results_['shift_cost'] = results_['duration'] * results_['hourly_rate']
+    
+    return results_
+    
+        
+        
+    
+    
+    
+    
+    
--- py/main.py
+++ py/main.py
@@ -12,12 +12,14 @@ Created on Sat Feb 22 15:59:34 2020
 import os
 import pandas as pd
 import numpy as np
-import pulp
+
 os.chdir('/Users/alexlamb/Desktop/Schedule_Opt/py')
 from helper import (generate_shifts,
                     generate_availability,
-                    generate_hours, 
-                    generate_hourly_need)
+                    gen_labor,
+                    optimize_shifts,
+                    optimize_assignment,
+                    format_results)
 
 # =============================================================================
 # Initialized data
@@ -44,86 +46,13 @@ avail_data = pd.read_csv('../data/availability.csv')
 labor = pd.read_csv('../data/labor_need.csv')
 
 # =============================================================================
-# Generate all possible shifts + availability
+# Generate all possible shifts + optimize across labor need
 # =============================================================================
 shifts = generate_shifts(schedule_start, schedule_end, min_shift, max_shift, open_closed)
+shifts = optimize_shifts(shifts, labor)
 avail = generate_availability(shifts, avail_data, employee_data)
-
-
-
-
-
-
-
-# =============================================================================
-# Manipulate data
-# =============================================================================
-
-hours = generate_hours(schedule_start, schedule_end)
-hours_avail = generate_availability(hours, employee_data, availability_data)
-labor_need = generate_hourly_need(hours, laborhour_data)
-hour_limits = hours_avail.reset_index()[['employee_name', 'min_hours', 'max_hours']].drop_duplicates().set_index('employee_name')
-
-timeslots = labor_need['timeslot']
-employees = hours_avail.index.levels[1]
-
-# =============================================================================
-# Set up optimization problem
-# =============================================================================
-prob = pulp.LpProblem('GenSchedule', pulp.LpMinimize) # Minimize costs
-
-scheduled = pulp.LpVariable.dicts('scheduled',
-                                ((timeslot, employee_name) for timeslot, employee_name in hours_avail.index),
-                                 lowBound=0,
-                                 cat='Binary')
-
-# Minimization function                           
-prob += pulp.lpSum(
-    [scheduled[timeslot, employee_name] * hours_avail.loc[(timeslot, employee_name), 'hourly_rate'] 
-    for timeslot, employee_name in hours_avail.index]
-)
-
-# Balance labor need with staffing
-for timeslot in timeslots:
-    prob += (sum([scheduled[(timeslot, employee)] for employee in employees]) 
-    == labor_need.loc[timeslot, 'labor_need'])
-
-# Check availability
-for timeslot in timeslots:
-    for employee in employees:
-        if hours_avail.loc[(timeslot, employee), 'avail'] == 0:
-            prob += scheduled[timeslot, employee] == 0
-
-# Check min hours
-for employee in employees:
-    prob += (sum([scheduled[(timeslot, employee)] for timeslot in timeslots])
-    >= hour_limits.loc[employee, 'min_hours']) 
-    
-# Check max hours
-for employee in employees:
-    prob += (sum([scheduled[(timeslot, employee)] for timeslot in timeslots])
-    <= hour_limits.loc[employee, 'max_hours']) 
-
-prob.solve()
-print(pulp.LpStatus[prob.status])
-
-output = []
-for timeslot, employee in scheduled:
-    var_output = {
-        'Timeslot': timeslot,
-        'Employee': employee,
-        'Scheduled': scheduled[(timeslot, employee)].varValue,
-    }
-    output.append(var_output)
-output_df = pd.DataFrame.from_records(output)#.sort_values(['timeslot', 'staffmember'])
-output_df.set_index(['Timeslot', 'Employee'], inplace=True)
-if pulp.LpStatus[prob.status] == 'Optimal':
-    print(output_df)
-
-
-
-
-
+results = optimize_assignment(shifts, avail)
+results = format_results(results, employee_data, shifts)
 
 
 
